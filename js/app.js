e8b6536853a11defd7e6dae1688e956c8a2255f3
Dana Marin <d_maringarcia@fanshaweonline.ca>
2019-07-22 00:40:35 -0400

    Mobile and desktop responsive design

--- js/app.js
+++ js/app.js
@@ -1,4 +1,16 @@
 (() => {
   console.log('yadayada');
 
+  let mobileNav = document.querySelector('.nav'),
+		navPanel = document.querySelector('#burgerCon');
+
+	function toggleNav() {
+		console.log('should show nav dropdown');
+		navPanel.classList.toggle('show-mobile-nav');
+	}
+
+
+
+	mobileNav.addEventListener('click', toggleNav);
+
 })();
